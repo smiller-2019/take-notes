de1986812a2d0a367fa3651d46d2bdebf426d9b9
Susan Miller <sjm_02@hotmail.com>
2022-09-20 16:26:42 +0100

    changed path to index.html and notes.html as still not working on heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,11 +3,15 @@ const path = require("path");
 
 const app = express();
 
+console.log(__dirname);
+
 app.use(express.static("public"));
 
 // GET Route for homepage
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "notes.html"))
+  res.sendFile(path.join(__dirname, "./../public/notes.html"))
 );
 
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "./../public/index.html"))
+);
