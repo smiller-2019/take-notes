adb4c516489f70c73459cec98117843c9c86c7d4
Susan Miller <sjm_02@hotmail.com>
2022-09-20 16:17:11 +0100

    change path of index.html and notes.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,9 +7,7 @@ app.use(express.static("public"));
 
 // GET Route for homepage
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/notes.html"))
+  res.sendFile(path.join(__dirname, "notes.html"))
 );
 
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/index.html"))
-);
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
