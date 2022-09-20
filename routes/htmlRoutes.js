1265115a967a2bf683a645ed8557eeea0a0a0eaa
Susan Miller <sjm_02@hotmail.com>
2022-09-20 16:32:49 +0100

    changed html paths again in htmlRoutes as still not working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,9 +9,9 @@ app.use(express.static("public"));
 
 // GET Route for homepage
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/notes.html"))
+  res.sendFile(path.join(__dirname, "../public/notes.html"))
 );
 
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/index.html"))
+  res.sendFile(path.join(__dirname, "../public/index.html"))
 );
